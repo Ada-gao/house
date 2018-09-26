539cbce851872200421d5647fe03e620c78bb3b6
gaoyuan <gaoyuan@shuyun365.com>
2018-09-26 09:32:31 +0800

    entrust page and radio component

---
+++ src/components/radio/radioButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="radio-group">
+    <input type="radio" name="one" label="是" value="0" />
+    <input type="radio" name="one" label="否" value="1" />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      imgUrls: [
+        
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
--- src/pages/entrust/index.vue
+++ src/pages/entrust/index.vue
@@ -1,7 +1,107 @@
 <template>
   <div class="container">
-    <swiper></swiper>
     <div class="main">
+      <div class="weui-cells__title">房产信息</div>
+      <div class="weui-cells weui-cells_after-title">
+        <div class="weui-cell weui-cell_input">
+          <div class="weui-cell__hd">
+            <div class="weui-label">手机号</div>
+          </div>
+          <div class="weui-cell__bd">
+            <input class="weui-input" placeholder="请输入手机号" />
+          </div>
+        </div>
+        <div class="weui-cell weui-cell_input">
+          <div class="weui-cell__hd">
+            <div class="weui-label">小区名称</div>
+          </div>
+          <div class="weui-cell__bd">
+            <input class="weui-input" placeholder=" " />
+          </div>
+        </div>
+        <div class="weui-cell weui-cell_input">
+          <div class="weui-cell__hd">
+            <div class="weui-label">楼号</div>
+          </div>
+          <div class="weui-cell__bd">
+            <input class="weui-input" placeholder=" " />
+          </div>
+          <div class="weui-cell__hd">
+            <div class="weui-label">房门号</div>
+          </div>
+          <div class="weui-cell__bd">
+            <input class="weui-input" placeholder=" " />
+          </div>
+        </div>
+        <div class="weui-cell weui-cell_input">
+          <div class="weui-cell__hd">
+            <div class="weui-label">委托类型</div>
+          </div>
+          <div class="weui-cell__bd">
+            <span class="radio-btn">出售</span>
+            <span class="radio-btn">出租</span>
+          </div>
+        </div>
+        <div class="weui-cell weui-cell_input">
+          <div class="weui-cell__hd">
+            <div class="weui-label">面积</div>
+          </div>
+          <div class="weui-cell__bd">
+            <input class="weui-input" placeholder=" " />
+          </div>
+          <div class="weui-cell__hd">
+            <div class="weui-label">户型</div>
+          </div>
+          <div class="weui-cell__bd">
+            <input class="weui-input" placeholder=" " />
+          </div>
+        </div>
+        <div class="weui-cell weui-cell_input">
+          <div class="weui-cell__hd">
+            <div class="weui-label">价格（元/月）</div>
+          </div>
+          <div class="weui-cell__bd">
+            <input class="weui-input" placeholder=" " />
+          </div>
+        </div>
+        <div class="weui-cell weui-cell_input">
+          <div class="weui-cell__hd">
+            <div class="weui-label">是否唯一</div>
+          </div>
+          <div class="weui-cell__bd">
+            <span class="radio-btn">是</span>
+            <span class="radio-btn">否</span>
+          </div>
+        </div>
+        <div class="weui-cell weui-cell_input">
+          <div class="weui-cell__hd">
+            <div class="weui-label">产证满几年</div>
+          </div>
+          <div class="weui-cell__bd">
+            <input class="weui-input" placeholder=" " />
+          </div>
+        </div>
+        <div class="weui-cell weui-cell_input">
+          <div class="weui-cell__hd">
+            <div class="weui-label w330">是否可随时看房</div>
+          </div>
+          <div class="weui-cell__bd">
+            <span class="radio-btn">是</span>
+            <span class="radio-btn">否</span>
+          </div>
+        </div>
+        <div class="weui-cell weui-cell_input">
+          <div class="weui-cell__hd">
+            <div class="weui-label w330">什么时候方便接听电话</div>
+          </div>
+          <div class="weui-cell__bd">
+            <picker mode="time" value="time" start="09:01" end="21:01" @change="bindTimeChange">
+              <div class="weui-input">{{time}}</div>
+            </picker>
+          </div>
+        </div>
+      </div>
+      <button class="weui-btn submit-btn" type="warn">提交</button>
     </div>
     <!-- <h5 class="recommend_title">位置及周边</h5>
     <h5 class="recommend_title">为你推荐</h5> -->
@@ -47,46 +147,31 @@ export default {
 <style lang="stylus" scoped>
 .main
   width 100%
-  padding 0 30rpx
-  box-sizing border-box
-  .split
-    border-bottom 1px solid #ccc
-  .roadInfo
-    padding 30rpx 0
-    p
-      font-size 28rpx
-  .houseInfo
-    display flex
-    padding 30rpx 0
-    .threeParam
-      flex 1
-      text-align center
-      .paramTitle
-        font-size 32rpx
-      .paramData
-        color #ce3e14
-        font-size 36rpx
-  .houseDetail
-    padding 30rpx 0
-    font-size 28rpx
-    .tag i
-      border 1px solid rgb(218, 178, 115)
-      color rgb(218, 178, 115)
-      display inline
-      padding 5rpx 8rpx
-      margin-right 10rpx
-      border-radius 4rpx
-    .item-detail
-      display flex
-      p
-        flex 1
-.recommend_title
-  font-size 36rpx
-  padding-left 60rpx
-  margin-top 20rpx
-  font-weight bold
-  text-align left
-  width 100%
+  padding 0 30rpx 130rpx
+  box-sizing border-border-box
+  .weui-cells
+    font-size 32rpx
+    .weui-cell_input
+      padding-top 20rpx
+      padding-bottom 20rpx
+      .weui-label.w330
+        width 330rpx
+      .weui-cell__bd
+        text-align right
+  .submit-btn
+    background-color #ce3e14
+    margin 60rpx 30rpx
+    border-radius 50rpx
+    height 100rpx
+    line-height 100rpx
+  .radio-btn
+    background-color rgb(230 230 230)
+    display inline
+    padding 10rpx 36rpx
+    border-radius 36rpx
+    outline-style none
+    color rgb(120 120 120)
+    margin-left 30rpx
 .footer
   display flex
   position fixed
